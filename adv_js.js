1e7cb885329476d302c68edc99624d67d9334977
19barsav <barne329@msu.edu>
2022-06-18 18:43:05 -0400

    aattempted own logic for opening and closing

--- adv_js.js
+++ adv_js.js
@@ -1,11 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+var truth = false;
+function toggleNav() {
+    if (truth) {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        truth = false;
+    }
+    else {
+        document.getElementById("mySidebar").style.width = "0px";
+        document.getElementById("main").style.marginLeft = "0px";
+        truth = true;
+    }
   }
   
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
