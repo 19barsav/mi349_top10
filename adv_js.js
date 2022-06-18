36865058420397fa6a0b17314b50c8c147d74bcd
19barsav <barne329@msu.edu>
2022-06-18 18:58:41 -0400

    gotta outsmart the code

--- adv_js.js
+++ adv_js.js
@@ -1,5 +1,5 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-var truth = false;
+var truth = true;
 function toggleNav() {
     if (truth) {
         document.getElementById("mySidebar").style.width = "250px";
@@ -7,8 +7,8 @@ function toggleNav() {
         truth = false;
     }
     else {
-        document.getElementById("mySidebar").style.width = "50px";
-        document.getElementById("main").style.marginLeft = "50px";
+        document.getElementById("mySidebar").style.width = "100px";
+        document.getElementById("main").style.marginLeft = "100px";
         truth = true;
         document.getElementsByClassName("tabs").style.display="none";
     }
