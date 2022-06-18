7e47b1b2f2f26023fc0a4999bba1f38dd14e22e2
19barsav <barne329@msu.edu>
2022-06-18 19:02:34 -0400

    maybe now

--- adv_js.js
+++ adv_js.js
@@ -1,9 +1,10 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-var truth = true;
+var truth = false;
 function toggleNav() {
     if (truth) {
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
+        document.getElementsByClassName("tabs").style.display="block";
         truth = false;
     }
     else {
