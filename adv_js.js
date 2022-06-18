e06de4af71830259f2f68cf597de28daa268fb02
19barsav <barne329@msu.edu>
2022-06-18 19:16:25 -0400

    i like this better

--- adv_js.js
+++ adv_js.js
@@ -1,24 +1,11 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-var truth = true;
-function toggleNav(open_2) {
-    if (truth && open_2) {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        truth = false;
-    }
-    else if (open_2) {
-        document.getElementById("mySidebar").style.width = "100px";
-        document.getElementById("main").style.marginLeft = "150px";
-        document.getElementsByClassName("tabs").style.display="none";
-        truth = true;
-       
-    }
-
-    else {
-        document.getElementById("mySidebar").style.width = "100px";
-        document.getElementById("main").style.marginLeft = "150px";
-        document.getElementsByClassName("tabs").style.display="none";
-        truth = false;
-    }
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
   }
   
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
