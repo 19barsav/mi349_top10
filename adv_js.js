d4b04cecc01ffecb14b0ce2b688824f56f5f2cc7
19barsav <barne329@msu.edu>
2022-06-18 19:08:57 -0400

    ahhh now i got it

--- adv_js.js
+++ adv_js.js
@@ -1,17 +1,21 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 var truth = true;
-function toggleNav() {
-    if (truth) {
+function toggleNav(open_2) {
+    if (truth && open_2) {
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
         truth = false;
     }
-    else {
+    else if (open_2) {
         document.getElementById("mySidebar").style.width = "100px";
-        document.getElementById("main").style.marginLeft = "100px";
+        document.getElementById("main").style.marginLeft = "150px";
         document.getElementsByClassName("tabs").style.display="none";
         truth = true;
        
+    }
+
+    else {
+        document.getElementsByClassName("tabs").style.display="none";
     }
   }
   
